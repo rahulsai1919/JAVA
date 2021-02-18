144acbe26e1b26f7a7ef32a8e005003b3a30da60
rahulsai1919 <55800414+rahulsai1919@users.noreply.github.com>
2021-02-18 15:38:05 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello {
+
+    public static void main(String[] args){
+        int [][] a , b[];
+        a = new int[5][5];
+        b = new int[2][2][2];
+        System.out.println(a);
+        System.out.println(b[0]);
+        System.out.println(new demo());
+
+    }
+
+}
+class demo
+{
+
+}
